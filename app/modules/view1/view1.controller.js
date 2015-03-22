9f4a6334ff56cd8202d09a2606c64893c1835e04
Kasper Emil Storgaard <kasperstorgaard@Kaspers-MBP.home>
2015-03-22 11:27:41 +0100

    added mount fn

--- app/modules/view1/view1.controller.js
+++ app/modules/view1/view1.controller.js
@@ -1,13 +1,18 @@
+var R = require('ramda');
+
 module.exports = function($scope, $log, $timeout){
     $scope.test = 'view1-test';
 
     $scope.searchPhrase = {};
 
-    var logValue = function logValue (value) {
+    var logValue = function logValue(value) {
 		$log.log(value);
 	}
 
-	$scope.$watch('searchPhase', function(value){
-		$scope.searchPhrase.stream.onValue(logValue);
+	var mount = R.once(function() {
+		var arr = $scope.searchPhrase.stream
+			.onValue(logValue);
 	});
+
+	$scope.$watch('searchPhase', mount);
 };
